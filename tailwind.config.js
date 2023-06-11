919ef14d4dd658c654ef28e4c63d64b30f5fdd72
Gregory Ross <gregross.dev@gmail.com>
2023-06-11 13:34:22 -0400

    add official tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+    ],
 }
 
