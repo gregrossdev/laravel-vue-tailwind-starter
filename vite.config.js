5fc692f1693ace065b713935368420039efec9bb
Gregory Ross <gregross.dev@gmail.com>
2023-06-14 17:40:49 -0400

    update @ alias in vite for vue3

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,29 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
         vue({
             template: {
                 transformAssetUrls: {
                     base: null,
                     includeAbsolute: false,
-                }
-            }
-        }),,
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+                },
+            },
         }),
     ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+            '@': '/resources/js/src',
+        },
+    },
 });
